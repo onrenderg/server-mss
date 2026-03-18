54c632d2e1ce8c908517833ae64da7e7c2c0960d
onrenderg <157473383+onrenderg@users.noreply.github.com>
2026-03-18 16:41:14 +0530

    1

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@
 APP_ID = "1BE74FZNXA"
 APP_TYPE = "100"
 SECRET_KEY = "P485IP4X2O"
-FY_ID = "XP12325"  # Add your Fyers ID here
+FY_ID = "XP"  # Add your Fyers ID here
 APP_ID_TYPE = "2"  # 2 denotes web login
 TOTP_KEY = "Q2HC7F57FHMHPRT2VRLPRWA4ORWPK34E"  # Add your TOTP secret key here
 PIN = "8894"  # Add your Fyers account PIN here
